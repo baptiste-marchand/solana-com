f47e8f1cd514fa969c9ddcada099f88635e3bdd1
raunakhajela <raunakhajela@gmail.com>
2024-12-26 21:05:09 +0530

    add lucide and pinned card

--- src/components/home/YHero.jsx
+++ src/components/home/YHero.jsx
@@ -1,11 +1,12 @@
 import styles from "@/components/home/YHero.module.scss";
 import Button from "@/components/solutions/Button";
-import { Zap, ArrowUpRight, Hexagon, Feather } from "react-feather";
+import { Zap, ArrowUpRight, Diamond, Leaf, Pin, Newspaper } from 'lucide-react';
+import Image from "next/image";
 
 const YHero = () => {
   return (
     <div className={styles.hero}>
-      <div className={styles.hero__content}>
+      <div className={styles.hero__container}>
         <div>
           <h1 className={styles.hero__title}>
             Fast
@@ -26,35 +27,53 @@ const YHero = () => {
             <span
               className={`${styles.hero__title_tag} ${styles.hero__title_tag__nodes}`}
             >
-              <Hexagon size="14" />
+              <Diamond size="14" />
               1,490 VALIDATOR NODES
             </span>
             Everyone
             <span
               className={`${styles.hero__title_tag} ${styles.hero__title_tag__carbon}`}
             >
-              <Feather size="14" />
+              <Leaf size="14" />
               0% NET CARBON IMPACT
             </span>
           </h1>
         </div>
 
-        <div className={styles.hero__info_group}>
-          <p className={styles.hero__description}>
-            Solana is a high-performance blockchain platform designed to support
-            decentralised applications and cryptocurrencies. It aims to provide
-            scalability without sacrificing decentralisation or security.
-          </p>
+        <div className={styles.hero__content}>
+          <div className={styles.hero__info_group}>
+            <p className={styles.hero__description}>
+              Solana is a high-performance blockchain platform designed to
+              support decentralised applications and cryptocurrencies. It aims
+              to provide scalability without sacrificing decentralisation or
+              security.
+            </p>
 
-          <div className={styles.hero__cta_group}>
-            <Button text="Start Building" url="/" />
+            <div className={styles.hero__cta_group}>
+              <Button text="Start Building" url="/" />
 
-            <a href="#" className={styles.hero__cta_btn_secondary}>
-              <span>
-                Resources
-                <ArrowUpRight size="14" />
-              </span>
-            </a>
+              <a href="#" className={styles.hero__cta_btn_secondary}>
+                <span>
+                  Resources
+                  <ArrowUpRight size="14" />
+                </span>
+              </a>
+            </div>
+          </div>
+
+          {/* pinned card */}
+          <div className={styles.hero__pinned_block}>
+            <div className={styles.hero__pinned_label}>
+              Pinned
+              <Pin size="14" />
+            </div>
+            <div className={styles.hero__pinned_card}>
+              <Image src="/img/pinned-image.jpg" alt="Pinned image" fill />
+              <div className={styles.hero__blurred_info}>
+                <span>Tickets on Sale for Breakpoint 2024</span>
+                <Newspaper size="24" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
