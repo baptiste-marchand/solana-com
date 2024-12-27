03b3d549ae0d3281c5d086509bc6cea1fb7cd8ca
raunakhajela <raunakhajela@gmail.com>
2024-12-27 16:10:10 +0530

    add random gradient bg

--- src/components/home/YHero.jsx
+++ src/components/home/YHero.jsx
@@ -1,13 +1,14 @@
 import styles from "@/components/home/YHero.module.scss";
 import Button from "@/components/solutions/Button";
-import { Zap, ArrowUpRight, Diamond, Leaf, Pin, Newspaper } from 'lucide-react';
+import { Zap, ArrowUpRight, Diamond, Leaf, Pin, Newspaper } from "lucide-react";
 import Image from "next/image";
 
 const YHero = () => {
   return (
     <div className={styles.hero}>
       <div className={styles.hero__container}>
-        <div>
+        <div className={styles.hero__inner_container}>
+
           <h1 className={styles.hero__title}>
             Fast
             <span
@@ -38,45 +39,49 @@ const YHero = () => {
               0% NET CARBON IMPACT
             </span>
           </h1>
-        </div>
 
-        <div className={styles.hero__content}>
-          <div className={styles.hero__info_group}>
-            <p className={styles.hero__description}>
-              Solana is a high-performance blockchain platform designed to
-              support decentralised applications and cryptocurrencies. It aims
-              to provide scalability without sacrificing decentralisation or
-              security.
-            </p>
+          <div className={styles.hero__content}>
+            <div className={styles.hero__info_group}>
+              <p className={styles.hero__description}>
+                Solana is a high-performance blockchain platform designed to
+                support decentralised applications and cryptocurrencies. It aims
+                to provide scalability without sacrificing decentralisation or
+                security.
+              </p>
 
-            <div className={styles.hero__cta_group}>
-              <Button text="Start Building" url="/" />
+              <div className={styles.hero__cta_group}>
+                <Button text="Start Building" url="/" />
 
-              <a href="#" className={styles.hero__cta_btn_secondary}>
-                <span>
-                  Resources
-                  <ArrowUpRight size="14" />
-                </span>
-              </a>
-            </div>
-          </div>
-
-          {/* pinned card */}
-          <div className={styles.hero__pinned_block}>
-            <div className={styles.hero__pinned_label}>
-              Pinned
-              <Pin size="14" />
-            </div>
-            <div className={styles.hero__pinned_card}>
-              <Image src="/img/pinned-image.jpg" alt="Pinned image" fill />
-              <div className={styles.hero__blurred_info}>
-                <span>Tickets on Sale for Breakpoint 2024</span>
-                <Newspaper size="24" />
+                <a href="#" className={styles.hero__cta_btn_secondary}>
+                  <span>
+                    Resources
+                    <ArrowUpRight size="14" />
+                  </span>
+                </a>
               </div>
             </div>
+
+            <div className={styles.hero__pinned_block}>
+              <div className={styles.hero__pinned_label}>
+                Pinned
+                <Pin size="14" />
+              </div>
+              <div className={styles.hero__pinned_card}>
+                <Image src="/img/pinned-image.jpg" alt="Pinned image" fill />
+                <div className={styles.hero__blurred_info}>
+                  <span>Tickets on Sale for Breakpoint 2024</span>
+                  <Newspaper size="24" />
+                </div>
+              </div>
+            </div>
+
           </div>
+
         </div>
       </div>
+
+      <div className={styles.hero__random_gradient_bg}></div>
+
     </div>
   );
 };
